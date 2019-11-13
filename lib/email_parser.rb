d174f75427fb50a764392186bb613ea3b2864ae6
Kevin Downs <kevin.downs04@gmail.com>
2019-11-13 22:49:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,11 +9,11 @@ class EmailAddressParser
   
   def initialize(email_csv)
     self.raw_email_csv = email_csv
-    self.delimiter = /[\s]/
+    self.delimiter = /[\s,]/
   end
 
   def parse
-    parsed_emails
+    self.parsed_emails = self.raw_email_csv.split(self.delimiter)
   end
 
 end
